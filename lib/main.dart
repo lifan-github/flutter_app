cfc6995c503f752890f0d687690f3048872dc56c
lf73051 <18266913301@163.com>
2021-07-07 19:24:18 +0800

    day1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'dart:developer';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 // 底部导航
 import 'router/TabNavigator.dart';
 // 启动页
@@ -18,32 +17,122 @@ import 'pages/user/user_detail_page.dart';
 
 void main() {
   runApp(
+    /// Providers are above [MyApp] instead of inside it, so that tests
+    /// can use [MyApp] while mocking the providers
     MultiProvider(
-      providers: [],
-      child: MyApp(),
-    )
+      providers: [
+        ChangeNotifierProvider(create: (_) => Counter()),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
 
-class MyApp extends StatelessWidget {
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       initialRoute: "starting_page",
+//       routes: {
+//         "starting_page": (context) => StartingPage(),
+//         "login_page": (context) => LoginPage(),
+//         "main_page": (context) => TabNavigator(),
+//         "home_detail_page": (context) => HomeDetailPage(),
+//         "cate_detail_page": (context) => CateDetailPage(),
+//         "user_detail_page": (context) => UserDetailPage(),
+//       },
+//       home: TabNavigator(),
+//     );
+//   }
+// }
+
+
+class Counter with ChangeNotifier, DiagnosticableTreeMixin {
+  int _count = 10;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+
+  /// Makes `Counter` readable inside the devtools by listing all of its properties
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(IntProperty('count', count));
+  }
+}
+
+class Count extends StatelessWidget {
+  const Count({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: "starting_page",
-      routes: {
-        "starting_page": (context) => StartingPage(),
-        "login_page": (context) => LoginPage(),
-        "main_page": (context) => TabNavigator(),
-        "home_detail_page": (context) => HomeDetailPage(),
-        "cate_detail_page": (context) => CateDetailPage(),
-        "user_detail_page": (context) => UserDetailPage(),
-      },
-      home: TabNavigator(),
+    return Text(
+
+      /// Calls `context.watch` to make [Count] rebuild when [Counter] changes.
+        '${context.watch<Counter>().count}',
+        key: const Key('counterState'),
+        style: Theme.of(context).textTheme.headline4);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('You have pushed the button this many times:'),
+
+            /// Extracted as a separate widget for performance optimization.
+            /// As a separate widget, it will rebuild independently from [MyHomePage].
+            ///
+            /// This is totally optional (and rarely needed).
+            /// Similarly, we could also use [Consumer] or [Selector].
+            Count(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: const Key('increment_floatingActionButton'),
+
+        /// Calls `context.read` instead of `context.watch` so that it does not rebuild
+        /// when [Counter] changes.
+        onPressed: () => context.read<Counter>().increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+
 
