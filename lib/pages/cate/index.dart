e661654dbea949fefa68016cc7cf450f7ce32ff0
lf73051 <18266913301@163.com>
2021-07-15 20:09:02 +0800

    listview

--- lib/pages/cate/index.dart
+++ lib/pages/cate/index.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const CITY_NAMES = ['北京', '上海', '广州', '杭州', '南京', '深圳', '山东'];
+
 class MyCatePage extends StatefulWidget {
   MyCatePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -9,30 +11,32 @@ class MyCatePage extends StatefulWidget {
 }
 
 class _MyCatePageState extends State<MyCatePage> {
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '资产1',
-            ),
-            FlatButton(
-              child: Text("open new route"),
-              textColor: Colors.blue,
-              onPressed: (){
-                Navigator.of(context).pushNamed("cate_detail_page", arguments: "我是上页面传过来的参数");
-              },
-            )
-          ],
-        ),
-      ),
+      body: ListView(
+        children: _buildListItem(),
+      )
     );
   }
+
+  // 列表渲染
+  List<Widget> _buildListItem(){
+    return CITY_NAMES.map((city) => _renderItem(city)).toList();
+  }
+
+  Widget _renderItem(city){
+    return Container(
+      height: 80,
+      margin: EdgeInsets.only(bottom: 5),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(color: Colors.blue),
+      child: Text(city, style: TextStyle(color: Colors.white, fontSize: 20)),
+    );
+  }
+
 }
